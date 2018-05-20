8c8d69912475ec6f9be1d014409f1a9cf73675fd
Yazer Azeez <y.azeez@cognisonus.com>
2018-05-20 16:44:50 +0530

    second commit

---
+++ src/face_eye_detection.py
@@ -0,0 +1,36 @@
+# Face Recognition using OpenCv and python
+
+# Importing the libraries
+import cv2
+
+# Loading the cascades haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('/home/dev03/yazer/GitHub/My Project/face_eye_detection/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('/home/dev03/yazer/GitHub/My Project/face_eye_detection/haarcascade_eye.xml')
+
+# Defining the function that will do the detection
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 5)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    return frame
+
+# Doing some face recognition with webcam
+video_capture = cv2.VideoCapture(0)
+while True:
+    __, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+    
+
+
